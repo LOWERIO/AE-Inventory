42b1977a9a7bea95def444c6ca79cbc6a3bb68aa
LOWERIO <ghostlowerIO@gmail.com>
2025-07-30 20:31:36 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-<script type="module">
+
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
@@ -24,4 +24,3 @@
   const auth = getAuth(app);
 
   export { db, auth };
-</script>
